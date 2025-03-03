0c5db69c3ef1f4883a532154b5d797a351e95453
JosephOlu <161716305+JosephOluOlofinte@users.noreply.github.com>
2025-03-03 17:27:12 +0100

    update

---
+++ app/homeComps/QuickAbout.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const QuickAbout = () => {
+  return (
+    <div>QuickAbout</div>
+  )
+}
+
+export default QuickAbout
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
 import Footer from '@/components/static/Footer'
 import Header from '@/components/static/Header'
 import React, { Fragment } from 'react'
+import QuickAbout from './homeComps/QuickAbout'
+import ProjectSlider from './homeComps/ProjectSlider'
+import Hero from './homeComps/hero'
+import Contact from './homeComps/Contact'
 
 const page = () => {
   return (
     <Fragment>
         <Header />
+        <Hero />
+        <QuickAbout />
+        <ProjectSlider />
+        <Contact />
         <Footer />
     </Fragment>
   )
