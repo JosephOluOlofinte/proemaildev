0ff75138e4925db4db0afa118140755b1f3db8b7
JosephOlu <161716305+JosephOluOlofinte@users.noreply.github.com>
2025-03-04 10:35:37 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,20 @@
-import Footer from '@/components/static/Footer'
+import { Fragment } from 'react'
 import Header from '@/components/static/Header'
-import React, { Fragment } from 'react'
+import Hero from './homeComps/Hero'
 import QuickAbout from './homeComps/QuickAbout'
 import ProjectSlider from './homeComps/ProjectSlider'
-import Hero from './homeComps/hero'
 import Contact from './homeComps/Contact'
+import Footer from '@/components/static/Footer'
 
 const page = () => {
   return (
     <Fragment>
-        <Header />
-        <Hero />
-        <QuickAbout />
-        <ProjectSlider />
-        <Contact />
-        <Footer />
+      <Header />
+      <Hero />
+      <QuickAbout />
+      <ProjectSlider />
+      <Contact />
+      <Footer />
     </Fragment>
   )
 }
