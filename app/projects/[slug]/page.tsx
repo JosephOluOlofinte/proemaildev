09c263061b4a91335a761f984256dae61a7c38f6
JosephOlu <161716305+JosephOluOlofinte@users.noreply.github.com>
2025-03-24 18:29:34 +0100

    update

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -1,20 +1,47 @@
-export {};
 
 import { emailProjectData } from "@/lib/emailProjectsData";
 import ProjectInfoCard from '@/components/block/ProjectInfoCard';
+import { Metadata } from "next";
+
 
 interface projectProps {
   params: Promise<{
     slug: string
   }>;
 }
+
+
+export async function generateMetadata({ params }: { params: { slug: string } }): Promise<Metadata> {
+  const data = emailProjectData.find((p) => p.slug === params.slug);
+
+  if (!data) {
+    return {
+      title: "Project Not Found",
+      description: "This project does not exist.",
+    };
+  }
+
+  return {
+    title: `${data.title} | Proemaildeveloper`,
+    description: data.desc,
+    openGraph: {
+      title: data.title,
+      description: data.desc,
+      images: [{ url: typeof data.desktopView === "string" ? data.desktopView : data.desktopView.src }],
+    },
+  };
+}
+
 const ProjectInfo = async ({ params }: projectProps) => {
 
   const { slug } = await params;
 
   const data = emailProjectData.find((item) => item.slug === slug)
 
-  return data ?  <ProjectInfoCard {...data} /> : 
+  return data ? 
+  <>
+    <ProjectInfoCard {...data} /> 
+  </> : 
   <>
     <main className="max-w-5xl h-screen my-auto mx-auto px-6 py-12">
       <h2>Aw, snap. The project you requested does not exist on this platform.</h2> <br />
--- components/block/SEO.tsx
+++ components/block/SEO.tsx
@@ -2,9 +2,20 @@
 
 import Head from 'next/head';
 import { SEOProps } from '@/lib/types';
+// import defaultSeoImage from '@/public/images/defaultSeoImage.png'
 
 
 const SEO = ({ title, description, keywords, image, url }: SEOProps) => {
+
+  const jsonLd = {
+    "@context": "https://schema.org",
+    "@type": "WebSite",
+    "name": title,
+    "url": url,
+    "image": image,
+    "description": description
+  };
+
   return (
     <Head>
       <title>{title}</title>
@@ -13,9 +24,10 @@ const SEO = ({ title, description, keywords, image, url }: SEOProps) => {
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
-      {image && <meta property="og:image" content={image} />}
+      {image && <meta property="og:image" content={typeof image === 'string' ? image : image.src} />}
       {url && <meta property="og:url" content={url} />}
       <meta name="robots" content="index, follow" />
+      <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }} />
     </Head>
   );
 };
